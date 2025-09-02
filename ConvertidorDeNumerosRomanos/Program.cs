68cb01a611f4efe0bc2f898af11d6261bd871be3
JesusBonilla9 <jesus_bonilla@ucne.edu.do>
2025-09-02 00:17:41 -0400

    Agregar archivos de proyecto.

---
+++ ConvertidorDeNumerosRomanos/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConvertidorDeNumerosRomanos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
